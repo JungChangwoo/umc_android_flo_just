faf366c8ccae4cb50ca88a6259be7865f3f5275a
jungwookim <rlawjddn103@naver.com>
2021-10-08 15:53:01 +0900

    add splash

---
+++ app/src/main/java/com/example/flo/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.flo
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        window?.decorView?.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        window.statusBarColor = Color.TRANSPARENT
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}
