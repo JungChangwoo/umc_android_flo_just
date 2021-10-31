f5ae2430de073a83905733b0e37db188cf9de64f
JungChangwoo <ikkwik02@naver.com>
2021-11-01 00:10:01 +0900

    add cardview

--- app/src/main/java/com/example/flo/AlbumRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/flo/AlbumRecyclerViewAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.flo.databinding.FragmentHomeBinding
 import com.google.gson.Gson
